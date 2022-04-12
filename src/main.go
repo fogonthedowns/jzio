d153193d7633cb77a478513a30959d58b143c933
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2022-04-12 12:57:13 -0700

    update

--- src/main.go
+++ src/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"jzio/src/internal/driver"
-	"jzio/src/internal/handler"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"jzio/src/internal/driver"
+	"jzio/src/internal/handler"
 
 	"github.com/go-chi/chi"
 	lru "github.com/hashicorp/golang-lru"
@@ -27,10 +30,40 @@ func main() {
 		os.Exit(-1)
 	}
 
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "data"))
+	assetsDir := http.Dir(filepath.Join(workDir, "assets"))
+	imgDir := http.Dir(filepath.Join(workDir, "img"))
+
+	FileServer(r, "/assets", assetsDir)
+	FileServer(r, "/img", imgDir)
+	FileServer(r, "/", filesDir)
+
 	r.Get("/post/{id:[0-9]+}", handler.GetPost(c, cache))
-	r.Get("/posts", handler.GetPosts(c, cache))
+	r.Get("/posts.json", handler.GetPosts(c, cache))
 
 	fmt.Println("Serving on " + port)
 	http.ListenAndServe(port, r)
 	defer c.SQL.Close()
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fmt.Println(pathPrefix)
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
